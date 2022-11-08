79134850385d8725c78cbce47f56e1f47483a63c
monumarquis <monumarquis@gmail.com>
2022-11-08 23:17:26 +0530

    BoilerPlate

--- email-app/src/index.js
+++ email-app/src/index.js
@@ -5,14 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider} from'@chakra-ui/react'
 import { Provider} from"react-redux"
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
+
+
+    {/* <Provider> */}
       <ChakraProvider>
           <App />
       </ChakraProvider>
-    </Provider>
+    {/* </Provider> */}
    
     
   </React.StrictMode>

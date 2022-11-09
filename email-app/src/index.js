dda7288ed6c1c283c7a7fe6a386a2793114564fb
vivek gupta <bittubieber19@gmail.com>
2022-11-09 21:22:02 +0530

    Day-2 Task Done

--- email-app/src/index.js
+++ email-app/src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider} from'@chakra-ui/react'
-import { Provider} from"react-redux"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
-      <ChakraProvider>
-          <App />
-      </ChakraProvider>
-    </Provider>
-   
-    
+    <App />
   </React.StrictMode>
 );
 
